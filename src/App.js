2aa456f03d7cac23b8795b9393622f0fd4d619d1
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-10-07 09:01:53 +0530

    'first-commit-in-covidApp'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CovidData from "./Components/Coviddata/CovidData";
+
+function App() {
+  return (
+    <>
+      <CovidData />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Coviddata/CovidData.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CovidData = () => {
+  //https://data.covid19india.org/v4/min/data.min.json this is api of covid
+  return (
+    <>
+      <h2>Welcome to find the detail of Covid</h2>
+      <button>Get Data</button>
+    </>
+  );
+};
+
+export default CovidData;
