5177537be13184592997fba9439f12ad9898dc1f
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-10-07 09:05:52 +0530

    'some-add-function'

--- src/Components/Coviddata/CovidData.jsx
+++ src/Components/Coviddata/CovidData.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 const CovidData = () => {
   //https://data.covid19india.org/v4/min/data.min.json this is api of covid
+
+  const clickBtn = () => {
+    console.log("testing btn ");
+  };
+
   return (
     <>
       <h2>Welcome to find the detail of Covid</h2>
-      <button>Get Data</button>
+      <button onClick={clickBtn}>Get Data</button>
     </>
   );
 };
