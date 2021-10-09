b371fa3fb75480389f16a0bd8070b8d7a24e0651
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-10-09 20:03:27 +0530

    class-component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import CovidData from "./Components/Coviddata/CovidData";
+import CovidDataC from "./Components/CovidDataC";
 
 function App() {
   return (
     <>
       <CovidData />
+      <CovidDataC />
     </>
   );
 }
---
+++ src/Components/CovidDataC.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CovidDataC extends Component {
+  render() {
+    return (
+      <>
+        <h4>this is a class base component</h4>
+      </>
+    );
+  }
+}
+
+export default CovidDataC;
