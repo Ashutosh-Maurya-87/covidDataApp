1d30b4c8e15ab3bb66a9de417e3790b34789dc4f
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-10-09 21:14:36 +0530

    class-component-add-some-function

--- src/Components/CovidDataC.jsx
+++ src/Components/CovidDataC.jsx
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 
 class CovidDataC extends Component {
   render() {
+    const btnFunc = () => {
+      console.log("chek THAT");
+    };
     return (
       <>
         <h4>this is a class base component</h4>
+        <button onClick={btnFunc}>Get Data</button>
       </>
     );
   }
